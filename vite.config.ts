644e5b43a6217b31564b947012b590795c48c28e
rschiku84-star <rschiku84@gmail.com>
2026-03-10 15:09:45 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react-swc"
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/market-mellow-maker/",
+  base: '/market-mellow-maker/',
   plugins: [react()],
-  build: {
-    outDir: "dist"
-  }
 })
