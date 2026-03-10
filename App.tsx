859f9e7804e0330a79c58f2c180d2be74cf71bdb
rschiku84-star <rschiku84@gmail.com>
2026-03-10 16:34:24 +0530

    Create App.tsx

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{textAlign:"center",marginTop:"100px"}}>
+      <h1>Market Mellow Maker</h1>
+      <p>Your AI Video Marketing App is Working 🚀</p>
+    </div>
+  );
+}
+
+export default App;
