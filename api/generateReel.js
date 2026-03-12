4e6da317942958a19b5bf675c8f8805e01ac17b6
rschiku84-star <rschiku84@gmail.com>
2026-03-12 18:38:30 +0530

    Update generateReel.js

--- api/generateReel.js
+++ api/generateReel.js
@@ -4,12 +4,23 @@ const reels = [
 "https://player.vimeo.com/external/403425024.sd.mp4",
 "https://player.vimeo.com/external/517956487.sd.mp4",
 "https://player.vimeo.com/external/449832689.sd.mp4"
-]
+];
 
-const random = reels[Math.floor(Math.random() * reels.length)]
+const hashtags = [
+"#viralreels","#instareels","#trending","#shopnow","#dealoftheday"
+];
+
+const thumbnail = "https://picsum.photos/600/400";
+
+const music = "https://cdn.pixabay.com/download/audio/2022/03/15/audio_c8c8a73467.mp3?filename=future-bass-117997.mp3";
+
+const randomVideo = reels[Math.floor(Math.random() * reels.length)];
 
 res.status(200).json({
-output: random
-})
+video: randomVideo,
+hashtags: hashtags.join(" "),
+thumbnail: thumbnail,
+music: music
+});
 
 }
