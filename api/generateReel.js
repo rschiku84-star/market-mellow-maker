c871e97b5ecf6425e05d10d2cb37c0670be0ece8
rschiku84-star <rschiku84@gmail.com>
2026-03-12 18:15:35 +0530

    Update generateReel.js

--- api/generateReel.js
+++ api/generateReel.js
@@ -1,9 +1,15 @@
 export default async function handler(req, res) {
 
-const video = "https://player.vimeo.com/external/403425024.sd.mp4"
+const reels = [
+"https://player.vimeo.com/external/403425024.sd.mp4",
+"https://player.vimeo.com/external/517956487.sd.mp4",
+"https://player.vimeo.com/external/449832689.sd.mp4"
+]
+
+const random = reels[Math.floor(Math.random() * reels.length)]
 
 res.status(200).json({
-output: video
+output: random
 })
 
 }
