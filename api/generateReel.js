40e19e4e455ee1bdecc95eaba54abdc4181fd0b7
rschiku84-star <rschiku84@gmail.com>
2026-03-12 20:26:11 +0530

    Update generateReel.js

--- api/generateReel.js
+++ api/generateReel.js
@@ -1,26 +1,22 @@
 export default async function handler(req, res) {
 
-const reels = [
-"https://player.vimeo.com/external/403425024.sd.mp4",
-"https://player.vimeo.com/external/517956487.sd.mp4",
-"https://player.vimeo.com/external/449832689.sd.mp4"
-];
+const r = await fetch(
+"https://api.pexels.com/videos/search?query=reels&per_page=3",
+{
+headers:{
+Authorization: process.env.PEXELS_KEY
+}
+})
 
-const hashtags = [
-"#viralreels","#instareels","#trending","#shopnow","#dealoftheday"
-];
+const data = await r.json()
 
-const thumbnail = "https://picsum.photos/600/400";
-
-const music = "https://cdn.pixabay.com/download/audio/2022/03/15/audio_c8c8a73467.mp3?filename=future-bass-117997.mp3";
-
-const randomVideo = reels[Math.floor(Math.random() * reels.length)];
+const video = data.videos[Math.floor(Math.random()*data.videos.length)]
 
 res.status(200).json({
-video: randomVideo,
-hashtags: hashtags.join(" "),
-thumbnail: thumbnail,
-music: music
-});
+video: video.video_files[0].link,
+thumbnail: video.image,
+hashtags: "#reels #viral #shopnow #trending",
+music: "https://cdn.pixabay.com/download/audio/2022/03/15/audio_c8cBa73467.mp3"
+})
 
 }
